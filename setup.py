5052d27393aac105f75360b02b8b78f55b2726db
Guillermo Alcantara <guimoAAGG@gmail.com>
2020-07-09 20:18:59 -0600

    Commitment undone
    Caused irregularities, the functions in main file.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='stego',
-      version='0.1.0-dev20200708',
+      version='0.1.0-dev20200709',
       description='Helper functions to download, unzip, and augment images for steganalysis',
       packages=['stego'],
       zip_safe=False)
